2d28dbc3e8fd2724e78ffdd788bee1ce9f54099f
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2022-12-10 16:10:29 +0600

    added mock tests 1

---
+++ src/test/java/simple/PositiveTests.java
@@ -0,0 +1,26 @@
+package simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+
+    @Test
+    public void posTest1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void posTest2(){
+        assertTrue(true);
+    }
+
+
+    @Test
+    public void posTest3(){
+        assertTrue(true);
+    }
+
+
+}
