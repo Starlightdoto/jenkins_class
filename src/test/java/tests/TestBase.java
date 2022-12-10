dfeb198fa5794e10ff4622bc339e49a546880712
AlmasN <95028680+Exiliahs@users.noreply.github.com>
2022-12-10 20:42:04 +0600

    added new config

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,10 +3,12 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 public class TestBase {
@@ -21,6 +23,11 @@ public class TestBase {
         Configuration.headless = false;
         Configuration.holdBrowserOpen = false;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
@@ -30,6 +37,9 @@ public class TestBase {
 
     @AfterEach
     public void addAttachments() {
-        
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
